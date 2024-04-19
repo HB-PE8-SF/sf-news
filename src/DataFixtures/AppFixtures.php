afee0357c4bddc515661fec8dc00388aa6d0ec2d
Lucas Delobelle <lucas@ld-web.net>
2024-04-19 14:38:13 +0200

    :sparkles: add Category fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Article;
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
@@ -11,9 +12,19 @@ class AppFixtures extends Fixture
 {
     private const NB_ARTICLES = 50;
 
+    private const CATEGORIES = ['Front-end', 'Back-end', 'Full-Stack', 'Framework', 'API', 'DevOps'];
+
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
+        $categories = [];
+
+        foreach (self::CATEGORIES as $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+            $categories[] = $category;
+        }
 
         for ($i = 0; $i < self::NB_ARTICLES; $i++) {
             $article = new Article();
@@ -21,7 +32,8 @@ class AppFixtures extends Fixture
                 ->setTitle($faker->realText(50))
                 ->setContent($faker->realTextBetween(500, 700))
                 ->setVisible($faker->boolean(80))
-                ->setCreatedAt($faker->dateTimeBetween('-2 years'));
+                ->setCreatedAt($faker->dateTimeBetween('-2 years'))
+                ->setCategory($faker->randomElement($categories));
 
             $manager->persist($article);
         }
