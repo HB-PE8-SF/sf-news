c4ff7c3f13dc396d4537163da851b2d63478f939
Lucas Delobelle <lucas@ld-web.net>
2024-05-02 15:10:25 +0200

    :sparkles: add Articles page

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -23,5 +24,13 @@ class IndexController extends AbstractController
     public function about(): Response
     {
         return $this->render('index/about.html.twig');
+    }
+
+    #[Route('/articles', name: 'app_articles')]
+    public function articles(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('index/articles.html.twig', [
+            'articles' => $articleRepository->findAll()
+        ]);
     }
 }
