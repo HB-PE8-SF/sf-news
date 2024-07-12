fca94bb75b05dc83fcd71892a738d2ca3c9a79ae
Lucas Delobelle <lucas@ld-web.net>
2024-07-12 11:22:23 +0200

    :sparkles: add Create admin command

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:create-admin',
+    description: 'Create an admin user in the database',
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(private EntityManagerInterface $em)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('email', InputArgument::REQUIRED, 'User email')
+            ->addArgument('password', InputArgument::REQUIRED, 'User password')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+        $email = $input->getArgument('email');
+        $password = $input->getArgument('password');
+
+        $user = new User();
+
+        $user
+            ->setEmail($email)
+            ->setPassword($password)
+            ->setRoles(['ROLE_ADMIN']);
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $io->success('Admin user successfully created');
+
+        return Command::SUCCESS;
+    }
+}
